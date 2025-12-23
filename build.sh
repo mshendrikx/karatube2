26eb635c1543aa046d76abc26763578152fabcfe
Maurício Hendrikx <mauricio.servatius@gmail.com>
2025-12-23 13:34:43 -0300

    main

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-docker compose -f /home/msh/docker/compose/docker-compose.yml down karatube2
+docker compose -f /home/msh/docker/compose/docker-compose.yml down karatube
 docker rmi mservatius/karatube2:latest
 docker build -t karatube2:latest .
 docker tag karatube2:latest mservatius/karatube2:latest
 docker push mservatius/karatube2:latest
 docker rmi karatube2:latest
 docker rmi mservatius/karatube2:latest
-docker compose -f /home/msh/docker/compose/docker-compose.yml up karatube2 -d
+docker compose -f /home/msh/docker/compose/docker-compose.yml up karatube -d
